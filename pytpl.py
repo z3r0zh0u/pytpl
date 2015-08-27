775ffa39c1d1426462d5b4c17701c13002f551c5
z3r0zh0u <littlefater2012@gmail.com>
2015-08-27 11:22:40 +0800

    Create pytpl.py

---
+++ pytpl.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Creaet Python template file.close
+
+Usage:
+    pytpl.py FileName
+"""
+
+import os
+import sys
+
+template = '''#!/usr/bin/env python
+"""
+Add your comments here.
+"""
+
+import os
+import sys
+
+if __name__ == '__main__':
+    """Main function"""
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if True == os.path.isfile(filename):
+            print filename
+        else:
+            print filename + ' is not a valid file.'
+    else:
+        print 'Usage: ' + sys.argv[0] + ' FileName'    
+
+'''
+
+if __name__ == '__main__':
+    """Main function"""
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if '.py' != os.path.splitext(filename)[-1]:
+                filename += '.py'
+        if False == os.path.exists(filename):
+            open(filename, 'wb').write(template)
+        else:
+            print filename + ' already exists.'
+    else:
+        print 'Usage: ' + sys.argv[0] + ' FileName'
