1e0c1a5a70856e1d86df0b1c89c030e71c1c2890
z3r0zh0u <littlefater2012@gmail.com>
2016-01-19 16:01:35 +0800

    Update pytpl.py

--- pytpl.py
+++ pytpl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Creaet Python template file.close
+Creaet Python template file
 
 Usage:
     pytpl.py FileName
@@ -16,19 +16,30 @@ Add your comments here.
 
 import os
 import sys
+import argparse
 
 if __name__ == '__main__':
-    """Main function"""
+    """main function"""
 
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-        if True == os.path.isfile(filename):
-            print filename
+    parser = argparse.ArgumentParser(description='add description here')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-d', '--dir', action='store_true', help='directory')
+    group.add_argument('-f', '--file', action='store_true', help='file')
+    parser.add_argument('target', action='store')
+
+    args = parser.parse_args()
+
+    if args.dir:
+        if True == os.path.isdir(args.target):
+            print args.target
         else:
-            print filename + ' is not a valid file.'
-    else:
-        print 'Usage: ' + sys.argv[0] + ' FileName'    
+            print '[!] Invalid Directory: ' + args.target
 
+    if args.file:
+        if True == os.path.isfile(args.target):
+            print args.target
+        else:
+            print '[!] Invalid File: '+ args.target 
 '''
 
 if __name__ == '__main__':
